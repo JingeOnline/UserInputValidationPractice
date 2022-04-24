a5404b674a7d0f8f8f734f09f136efdc52159e13
Jinge Zhang <jinge_windows@hotmail.com>
2022-04-24 22:10:25 +1000

    添加了一些注释。

--- UserInputValidationPractice/Models/Truck.cs
+++ UserInputValidationPractice/Models/Truck.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.ComponentModel;
 using Microsoft.Practices.EnterpriseLibrary.Validation;
 using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+using System.Diagnostics;
 //using System.ComponentModel.DataAnnotations;
 
 namespace UserInputValidationPractice.Models
@@ -15,11 +16,12 @@ namespace UserInputValidationPractice.Models
     /// 项目中使用的Microsoft.Practices.EnterpriseLibrary.Validation太老了，已经过时了。
     /// 1. 初始化的时候就可以验证对象的所有属性
     /// 2. 先赋值再验证，即使验证失败，值也会被set到属性中
+    /// 3. 如果是用户输入类型错误，也就是Exception类型的错误，ValidationResults中并不会接收。因此如果要控制CanExecute，还需要单独获取Exception类型的错误。
     /// </summary>
     public class Truck: BindableBase, IDataErrorInfo
     {
         private int _id;
-        //[Required]
+
         [NotNullValidator(MessageTemplate = "Id is required.")]
         public int Id
         {
@@ -28,7 +30,7 @@ namespace UserInputValidationPractice.Models
         }
 
         private string _productName;
-        //[StringLength(5, MinimumLength = 3)]
+
         [StringLengthValidator(3,5, MessageTemplate ="Product name length is only allowed between 3 - 5.")]
         public string ProductName
         {
@@ -37,7 +39,7 @@ namespace UserInputValidationPractice.Models
         }
 
         private DateTime _pickupDate;
-        //[Range(typeof(DateTime), "1/4/2022", "30/4/2022")]
+
         [PropertyComparisonValidator(nameof(DeliverDate),ComparisonOperator.LessThanEqual)]
         public DateTime PickupDate
         {
@@ -46,7 +48,7 @@ namespace UserInputValidationPractice.Models
         }
 
         private DateTime _deliverDate;
-        //[Range(typeof(DateTime), "1/4/2022", "30/4/2022")]
+
         [PropertyComparisonValidator(nameof(PickupDate), ComparisonOperator.GreaterThanEqual)]
         public DateTime DeliverDate
         {
@@ -55,7 +57,7 @@ namespace UserInputValidationPractice.Models
         }
 
         private double _min_Temp;
-        //[Range(0.0, 100.0)]
+
         [RangeValidator(0.0,RangeBoundaryType.Inclusive,100.0,RangeBoundaryType.Inclusive)]
         public double Min_Temp
         {
@@ -64,7 +66,7 @@ namespace UserInputValidationPractice.Models
         }
 
         private double _max_Temp;
-        //[Range(0.0, 100.0)]
+
         [RangeValidator(0.0, RangeBoundaryType.Inclusive, 100.0, RangeBoundaryType.Inclusive)]
         public double Max_Temp
         {
@@ -73,13 +75,13 @@ namespace UserInputValidationPractice.Models
         }
 
 
-
+        
         public string Error
         {
+            //把当前对象的所有错误信息字符串连在一起，返回。
             get
             {
                 StringBuilder error = new StringBuilder();
-
                 ValidationResults results = Validation.ValidateFromAttributes<Truck>(this);
                 foreach (var result in results)
                 {
@@ -88,22 +90,32 @@ namespace UserInputValidationPractice.Models
                 return error.ToString();
             }
         }
-
-        public string this[string columnName]
+        
+        //传入属性名称，返回该属性的错误信息。任意控件的PropertyChanged事件都会触发该“所引器”。
+        public string this[string propertyName]
         {
             get
             {
+                //results对象储存着该对象所有属性的错误。
                 ValidationResults results = Validation.ValidateFromAttributes<Truck>(this);
+                Debug.WriteLine("results.Count="+results.Count);
                 foreach (var result in results)
                 {
-                    if (result.Key == columnName)
+                    if (result.Key == propertyName)
                     {
+                        //返回的result.Message就是显示在UI上的错误提示信息。
                         return result.Message;
                     }
                 }
 
                 return string.Empty;
             }
+        }
+
+        //返回该对象是否所有字段都有效
+        public bool IsValid
+        {
+            get { return Validation.ValidateFromAttributes<Truck>(this).IsValid; }
         }
     }
 }
--- UserInputValidationPractice/ViewModels/TruckViewModel.cs
+++ UserInputValidationPractice/ViewModels/TruckViewModel.cs
@@ -3,6 +3,7 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
